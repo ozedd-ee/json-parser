ec9fa693f80ee9e91f95073bba95f8920c651359
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2024-04-17 15:22:52 +0100

    Add token type definitions and constants

---
+++ src/token.go
@@ -0,0 +1,29 @@
+package main
+
+const (
+	QUOTE      = `"`
+	LEFTBRACE  = "{"
+	RIGHTBRACE = "}"
+	LEFTBRACKET = "["
+	RIGHTBRACKET = "]"
+	WHITESPACE = " "
+	COMMA = ","
+	COLON = ":"
+)
+
+var STRUCTURAL_TOKENS = []string{"{", "}", ":", ",", "[", "]"}
+
+var VALID_NUMBER_CHAR = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "."}
+
+const TRUE_LEN = 4
+const FALSE_LEN = 5
+const NIL_LEN = 3
+
+type token struct {
+	value interface{}
+	position int
+}
+
+type node struct {
+	value interface{}
+}
