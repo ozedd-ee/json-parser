df45b8b9788e88e1bca19839a3d1357a3ad93df7
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2024-10-24 00:49:38 +0100

    feat: support multi-line JSON files as input chore: refactor for clarity

--- cmd/root.go
+++ cmd/root.go
@@ -17,10 +17,12 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/ozedd-ee/json-parser/src"
 	"github.com/spf13/cobra"
@@ -37,24 +39,25 @@ var rootCmd = &cobra.Command{
 	Example: "json-parser \"test.json\"  \njson-parser -l \"test.json\"",
 	Run: func(cmd *cobra.Command, args []string) {
 		file := args[0]
+        // Check if input is a JSON file
 		if file[len(file)-5:] != ".json" {
 			log.Fatal("Expected a JSON file as argument ")
 		}
-		fileContent, err := os.ReadFile(file)
+		openFile, err := os.Open(file)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("Error opening file:", err)
 		}
-		tokens := src.Lex(string(fileContent))
+		defer openFile.Close()
+        // Remove carriage returns before passing to lexer
+		fileContent := removeCarriageReturns(openFile)
+
+		tokens := src.Lex(fileContent)
 		if onlyLex {
 			fmt.Printf("TOKENS:\n %v \n \n", tokens)
 		} else {
 			abstractSyntaxTree, _ := src.Parse(tokens)
-			jsonEncodedAST, err := json.MarshalIndent(abstractSyntaxTree, "", " ")
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Printf("AST:\n %v \n \n", string(jsonEncodedAST))
-			}
+			jsonEncodedAST := marshalJSON(&abstractSyntaxTree)
+			fmt.Printf("AST:\n %v \n \n", jsonEncodedAST)
 		}
 	},
 }
@@ -89,4 +92,30 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
+}
+
+func removeCarriageReturns(openFile *os.File) string {
+	scanner := bufio.NewScanner(openFile)
+	var cleanedLines []string
+
+	// Read the file line by line
+	for scanner.Scan() {
+		line := scanner.Text()
+		// Remove carriage return characters but leave newline (\n)
+		cleanedLine := strings.ReplaceAll(line, "\r", "")
+		cleanedLines = append(cleanedLines, cleanedLine)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error reading file:", err)
+	}
+	fileContent := strings.Join(cleanedLines, "")
+	return fileContent
+}
+
+func marshalJSON(abstractSyntaxTree *src.Node) string {
+	jsonEncodedAST, err := json.MarshalIndent(abstractSyntaxTree, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(jsonEncodedAST)
 }
